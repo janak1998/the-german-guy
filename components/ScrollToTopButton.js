0869942007275136a632da345354f282cfede264
janak bohra <janakbohra29@gmail.com>
2024-10-03 11:16:29 +0530

    scrollTopBtn color changed

--- components/ScrollToTopButton.js
+++ components/ScrollToTopButton.js
@@ -15,7 +15,7 @@ const ScrollToTopButton = () => {
       className="fixed bottom-6 right-6 p-4 bg-blue-500 rounded-full shadow-lg hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-300 transition-all duration-300"
     >
       <svg
-        className="stroke-white fill-white dark:stroke-black dark:fill-black"
+        className="stroke-white fill-white "
         xmlns="http://www.w3.org/2000/svg"
         width="30"
         height="30"
