6876d5c1d02871fbfb6fec8934eb84d869f6ee4c
Awanish Srivastava <40716261+imawanish5@users.noreply.github.com>
2025-09-19 00:10:48 +0200

    Update page.js

--- app/impressum/page.js
+++ app/impressum/page.js
@@ -5,7 +5,7 @@ const page = () => {
     <div className="flex justify-center items-center h-[600px] px-4 py-10">
         <div className="flex flex-col mb-10 text-xl text-left text-gray-800 dark:text-gray-200 w-fit gap-5">
           <h1 className="text-4xl font-semibold mb-8 text-center">Impressum</h1>
-          <p>The German Guy</p>
+          <p>TGG(The German Guy)</p>
           <p>Mühlenweg 34a</p>
           <p>15232 Frankfurt(Oder), Germany</p>
           <p>Email: <a href="mailto:contact@thegermanguy.org" className="underline">contact@thegermanguy.org</a></p>
