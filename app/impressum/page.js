76ce00837da5c901432c259cb4b4eef9e4af8ee8
Awanish Srivastava <40716261+imawanish5@users.noreply.github.com>
2025-12-03 21:37:17 +0100

    .

--- app/impressum/page.js
+++ app/impressum/page.js
@@ -14,7 +14,7 @@ const page = () => {
           <p>Registergericht: Amtsgericht Frankfurt(Oder)</p>
           <p>Registernummer: HRB 21884 FF</p>
           <p>Umsatzsteuer-Identifikationsnummer: DE415329353</p>
-          <p>USt-IdNr. 061/276/01885</p>
+          <p>Steuernummer: 061/276/01885</p>
           <p>Geschäftsführer: <a href="https://www.linkedin.com/in/imawanish5/" target="_blank">Awanish Srivastava</a></p>
           </div>
     </div>
