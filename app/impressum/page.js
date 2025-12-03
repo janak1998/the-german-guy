e030cc62233514809d3641b5f128bdbd4b521540
Awanish Srivastava <40716261+imawanish5@users.noreply.github.com>
2025-12-03 21:45:08 +0100

    .

--- app/impressum/page.js
+++ app/impressum/page.js
@@ -15,6 +15,8 @@ const page = () => {
           <p>Registernummer: HRB 21884 FF</p>
           <p>Umsatzsteuer-Identifikationsnummer: DE415329353</p>
           <p>Steuernummer: 061/276/01885</p>
+          </div>
+          <div
           <Button className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded transition-colors max-w-[200px]">
           <p>Geschäftsführer: <a href="https://www.linkedin.com/in/imawanish5/" target="_blank">Awanish Srivastava</a></p>
           </Button>
